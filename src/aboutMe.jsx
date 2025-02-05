9b03d2ad6cba464c3d5adceb826b176f8f672b37
Caleb Colangelo <calebc041406@gmail.com>
2025-02-05 17:18:34 -0500

    started dev and finished the page layout

---
+++ src/aboutMe.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+const aboutMeButton = document.getElementById(aboutMe)
+
+function aboutMe() {
+    return (
+        <>
+            <main>
+                <body>
+                    <p>Hello!</p>
+                </body>
+            </main>
+        </>
+    )
+}
+
+export default aboutMe
